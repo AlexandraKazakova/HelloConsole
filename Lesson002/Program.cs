c177b79d9bfae67cd4f28900c6ffb13e99d53ae3
Alexandra <sasha_huliganka@mail.ru>
2022-04-25 21:39:59 +0300

    Lesson002_bool

--- Lesson002/Program.cs
+++ Lesson002/Program.cs
@@ -3,4 +3,4 @@ int numA = int.Parse(Console.ReadLine());
 Console.Write("Введите целое число -> ");
 int numB = int.Parse(Console.ReadLine());
 bool resalt = numB * numB == numA;
-Console.WriteLine(resalt);
+Console.WriteLine(resalt ? "Yes" : "No");
