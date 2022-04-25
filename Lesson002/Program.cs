25201c6914f40f82987ae3bc166e2f937e9e46bf
Alexandra <sasha_huliganka@mail.ru>
2022-04-25 21:38:27 +0300

    Lesson002

---
+++ Lesson002/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите целое число -> ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите целое число -> ");
+int numB = int.Parse(Console.ReadLine());
+bool resalt = numB * numB == numA;
+Console.WriteLine(resalt);
