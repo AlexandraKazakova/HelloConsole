9693a6db5db25fcfd42cccd2a16da300bfe0826e
Alexandra <sasha_huliganka@mail.ru>
2022-04-27 22:24:53 +0300

    Ex008

---
+++ Example008_Find/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {3,4,67,23,43,4,87,42};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+	if (array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+	index++;
+}
