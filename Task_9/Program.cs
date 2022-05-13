f10ea9cacdf6c764c39032b9869096f558d504d2
Alexandra <sasha_huliganka@mail.ru>
2022-05-13 17:49:10 +0300

    task_9.2

--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine($"Max = {maxNum}");
 
 int FindMaxNumber (int num)
 {
-	int firstDigit = number / 10;
-	int secondDigit = number % 10;
+	int firstDigit = num / 10;
+	int secondDigit = num % 10;
 	return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
